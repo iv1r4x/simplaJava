f4582c2180a2c5e99bbec1a9e4ee57511b3753e2
vivek razdan <vivek.razdan@exodusintel.com>
2019-04-02 17:35:40 +0530

    1

--- src/main/java/com/vivz/helloworld.java
+++ src/main/java/com/vivz/helloworld.java
@@ -2,6 +2,6 @@ package com.vivz;
 
 public class helloworld {
     public static void main(final String[] args) {
-        System.out.println("Hello, World from lonewolf!");
+        System.out.println("Hello, World from lonewolf222!");
     }
 }
