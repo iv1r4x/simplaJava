b63b0ce1f75222ca7698dad6e52393234aa3f212
vivek razdan <vivek.razdan@exodusintel.com>
2019-04-04 10:38:11 +0530

    added addition function

--- src/main/java/com/vivz/helloworld.java
+++ src/main/java/com/vivz/helloworld.java
@@ -3,14 +3,24 @@ package com.vivz;
 public class HelloWorld{
 
      public static void main(String []args){
-       String test = "hi my name is test and i want to be cool";
+       String test = "hi my name is test";
        String testcopy = test;
        printMessage(testcopy);
+       addNumbers(12,33);
      }
      
       public static void printMessage(String msg)
       {
           System.out.println(msg);
          
+      }
+      
+      public static void addNumbers(int x, int y)
+      {
+        int a=x;
+        int b=y;
+        int z=x+y;
+
+        System.out.println("Sum of x+y = " + z);
       }
 }
